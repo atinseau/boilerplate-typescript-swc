197160a53153959d25be11405b537ae2d900d9aa
Arthur Tinseau <arthur@mbp-de-arthur.home>
2022-08-11 12:04:44 +0200

    boilertemplate swc sidecar and typescript

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,7 @@
-import Express from "express";
+import express from "express";
 import './config';
-import { authRouter } from "./routes/auth";
-import { definitionRouter } from "./routes/definition";
-import { wordRouter } from "./routes/word";
 const PORT = process.env.PORT || 3000;
-const app = Express();
-app.use(Express.json());
-app.use(Express.urlencoded({
-    extended: true
-}));
-app.use('/auth', authRouter);
-app.use('/word', wordRouter);
-app.use('/definition', definitionRouter);
+const app = express();
 app.get('/', (req, res)=>{
     res.send(process.env.ENDPOINT);
 });
