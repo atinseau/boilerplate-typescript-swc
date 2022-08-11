6058e9912d9e1455e6796315347a11a3bddccb7c
Arthur Tinseau <arthur@mbp-de-arthur.home>
2022-08-11 12:09:25 +0200

    clear project

--- srcs/index.ts
+++ srcs/index.ts
@@ -1,14 +1,1 @@
-import express from "express";
-
-import './config'
-
-const PORT = process.env.PORT || 3000
-
-const app = express()
-
-app.get('/', (req, res) => {
-	res.send(process.env.ENDPOINT)
-})
-
-
-app.listen(PORT)
+console.log("Hello World");
