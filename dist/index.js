a0bd042030df3e5858561ebb298a707839b118ee
Arthur Tinseau <arthur@mbp-de-arthur.home>
2022-08-11 12:05:51 +0200

    scynr

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import './config';
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.get('/', (req, res)=>{
-    res.send(process.env.ENDPOINT);
-});
-app.listen(PORT);
